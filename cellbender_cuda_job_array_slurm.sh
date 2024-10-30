4ebd877366e8bc67c0937560514e4d4284e53a91
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-10-30 11:09:38 -0600

    Update cellbender_cuda_job_array_slurm.sh

--- cellbender_cuda_job_array_slurm.sh
+++ cellbender_cuda_job_array_slurm.sh
@@ -6,14 +6,15 @@
 #SBATCH --partition=aa100 # This is the name of the NVIDIA GPU partition. Made of nodes containing 3x A100 gpus.
 #SBATCH --gres=gpu:1 # Here we are requesting 1 gpu
 #SBATCH --job-name=cellbender_gpu # Name of the job that will be submitted.
-#SBATCH --output=cellbender_gpu.%j.out
-#SBATCH --error=cellbender_gpu.%j.err
-#SBATCH --mail-type=BEGIN,FAIL,END # Feel free to change it.
+#SBATCH --output=cellbender_gpu.%j.out # Name of the file where all the benign outputs and logs related to the run will be redirected. %j is the variable that will capture the jobID
+#SBATCH --error=cellbender_gpu.%j.err # Name of the file where all the errors related to the run will be redirected.
+#SBATCH --mail-type=BEGIN,FAIL,END #  I get the Slurm notification in my email inbox when it begins, ends and fails.
 # Whom to send the email to
-#SBATCH --mail-user=foo.foo@cuanschutz.edu
-#SBATCH --array=1-72 #Call for the job array with 72 indexes representing each job for the 72 samples 
+#SBATCH --mail-user=foo.foo@cuanschutz.edu # My email address where I wish to get all the notifications.
+#SBATCH --array=1-72 # Call for the job array with 72 indexes representing each job for the 72 samples 
 
-
+# /home filesystem is very small so we need to redirect tmp and cache related files to 
+# the scratch filesystem.
 export TMP=/gpfs/alpine1/scratch/$USER/cache_dir
 export TEMP=$TMP
 export TMPDIR=$TMP
@@ -21,6 +22,7 @@ export TEMPDIR=$TMP
 export PIP_CACHE_DIR=$TMP
 mkdir -pv $TMP
 
+# Loading my anaconda module and activating my conda ENV
 module load anaconda
 conda activate cellbender
 
